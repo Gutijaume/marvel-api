1b8651884f63bbb5842736989796313f153da258
gutijaume <gutijaume@hotmail.com>
2022-01-06 15:35:00 +0100

    modificando parametros

--- JS/marvelscript.js
+++ JS/marvelscript.js
@@ -1,4 +1,4 @@
-alert("Bienvenido a mi mundo Marvel")
+// alert("Bienvenido a mi mundo Marvel")
 
 window.onload = () => {
   const apiURL =  "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=ed225665a40d7a6a19ac6ac9b8bbb2b0&hash=6cc60c6f6c49d74a50e03009d3682b00"
@@ -74,64 +74,7 @@ window.onload = () => {
       }
     })
 }
-    "https://gateway.marvel.com:443/v1/public/characters?modifiedSince=2021&ts=1apikey=ed225665a40d7a6a19ac6ac9b8bbb2b0https://gateway.marvel.com:443/v1/public/characters?modifiedSince=2021&apikey=ed225665a40d7a6a19ac6ac9b8bbb2b0"
-
-// {/* <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> */}
-
- // let characterDiv = document.querySelector(".container")
-    // let idP = document.createElement("p")
-    // idP.innerText = object.id
-    // characterDiv.appendChild(idP)
-    // let h2Name = document.createElement("h2")
-    // h2Name = innerText = object.name
-    // // characterDiv.appendChild(h2Name)
-    // // let h2Wiki = document.createElement("h2")
-    // // h2Wiki.innerText = object.wiki
-    // // characterDiv.appendChild(h2Wiki)
-    // let imgCharacter = document.createElement("img")
-    // imgCharacter.src = object.img
-    // characterDiv.appendChild(imgCharacter)
-
-
-
-        // console.log(json)
-      //   // printToDom(json)
-      // })
   
-
-      
-
-
-//       fetch(urlAPI)
-//       .then((res) => res.json())
-//       .then((json) => {
-//         for (const hero of json.data.results) {
-//           let urlHero = hero.urls[0].url;
-//           contentHTML += ` <div class="col-md-4">
-//             <a href="${urlHero}" target="_blank">
-//                 <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
-//             </a>
-//             <h3 class="title">${hero.name}/h3>
-//         </div>`;
-//         }
-//         container.innerHTML = contentHTML
-//       });
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

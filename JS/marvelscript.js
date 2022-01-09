bb1c039d9c3ac88287404cceee19e2d1b00b600a
gutijaume <gutijaume@hotmail.com>
2022-01-09 16:48:52 +0100

    Último cambio antes de presentarlo
    Finalmente, lo dejo así para poder probar otros proyectos. La idea es ir mejorándolo a medida que avance el curso y resuelva algunas dudas

--- JS/marvelscript.js
+++ JS/marvelscript.js
@@ -10,25 +10,28 @@ window.onload = () => {
       //Debido a que algunas imagenes no están disponibles, filtro solo a los personajes con una imágen válida
       let withAvailableImg = json.data.results.filter(
         (item) => !item.thumbnail.path.includes("available")
-      );
-      let firstPag = withAvailableImg.slice(0, 8); //Por rendimiento de la web solo incluiré 8 carácteres
-      // let orderedById = (a, b) => a.id - b.id
+      )
+
+      
+      // let firstPag = withAvailableImg.slice(0, 12); //Por rendimiento de la web podríamos escoger cuales mostrar, y hacer varias paginas
+      
+      
+      // let orderedById = (a, b) => a.id - b.id //Cambiar el orden por id en vez de por nombre
       // firstPag.sort(orderedById);
       // console.log(firstPag);
 
       //Me decido por un "for of" para iterar con todos los heroes
-      for (const hero of firstPag) {
+      for (const hero of withAvailableImg) {
         console.log(hero);
 
         //Creamos DIV padre que contendra cada tarjeta
         let fatherDiv = document.querySelector(".card-group");
         let divContainer = document.createElement("div");
-        divContainer.setAttribute("class", "col-sm-6");
+        divContainer.setAttribute("class", "col-sm-6");        
         let characterDiv = document.createElement("div");
-        characterDiv.setAttribute("class", "card");
+        characterDiv.setAttribute("class", "card");        
         characterDiv.setAttribute("class", "card border-primary mb-3");
         characterDiv.setAttribute("class", "card text-center");
-        // characterDiv.setAttribute("class", "divClass")
         fatherDiv.append(divContainer);
         divContainer.appendChild(characterDiv);
 
@@ -55,68 +58,32 @@ window.onload = () => {
         characterDiv.appendChild(imgCharacter);
 
         //Creamos un botón que enlace con su wiki
-
         let wikiBtn = document.createElement("button");
+
+
+      //NO HE CONSEGUIDO QUE SE ABRA EL COLLAPSE Y MUESTRE LA INFO, EL BOTON NO FUNCIONA
+      // let description = document.createElement ("button")        
+      //   description.innerHTML =`<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      //   Haz clic para mostrar y ocultar el contenido
+      // </button>
+      // <div class="collapse" id="collapseExample">
+      //   <div class="card card-body">
+      //     ${hero.description}
+      //   </div>
+      // </div>`
         wikiBtn.setAttribute("class", "btn btn-primary");
         wikiBtn.innerHTML = `<a href="${hero.urls[0].url}" class="btn btn-primary btn-lg btn-block">CONOCE MAS SOBRE TU PERSONAJE</a>`;
         characterDiv.appendChild(wikiBtn);
-      }
+      // characterDiv.appendChild(description)
 
-      let btnId = document.querySelector('[data-function ="idOrder"]');
-      btnId.addEventListener("click", () => {
-        setNewOrder();
-      });
-      let setNewOrder = () => {
-        let orderedById = (a, b) => a.id - b.id ;
-        firstPag.sort(orderedById)
-        let newArray = [...firstPag]
+        //Choose your character (Por desarrollar, que podríamos hacer con esto??)
+        let selectionCharacter = document.querySelector("select")
+        let newOption = document.createElement("option")
+        newOption.innerHTML = `<option>${hero.name}</option>`
+        selectionCharacter.appendChild(newOption)
+
         
+     }
 
-        for (const hero of newArray){
-          console.log(hero);
-  
-          //Creamos DIV padre que contendra cada tarjeta
-          let fatherDiv = document.querySelector(".card-group");
-          let divContainer = document.createElement("div");
-          divContainer.setAttribute("class", "col-sm-6");
-          let characterDiv = document.createElement("div");
-          characterDiv.setAttribute("class", "card");
-          characterDiv.setAttribute("class", "card border-primary mb-3");
-          characterDiv.setAttribute("class", "card text-center");
-          // characterDiv.setAttribute("class", "divClass")
-          fatherDiv.append(divContainer);
-          divContainer.appendChild(characterDiv);
-  
-          //Incluimos el ID en formato h3
-          let idSubtitle = document.createElement("h3");
-          idSubtitle.setAttribute("class", "card-subtitle");
-          idSubtitle.innerText = hero.id;
-          characterDiv.appendChild(idSubtitle);
-  
-          //Incluimos el nombre del personaje en h1
-          let h2Name = document.createElement("h1");
-          h2Name.setAttribute("class", ".card-title");
-          h2Name.setAttribute("class", "h2Typo");
-          h2Name.innerText = hero.name;
-          characterDiv.appendChild(h2Name);
-  
-          //Incluimos las imágenes de cada personaje
-          let imgCharacter = document.createElement("img");
-          imgCharacter.src = hero.thumbnail.path + ".jpg";
-          imgCharacter.alt = hero.name;
-          imgCharacter.setAttribute("class", "card-img-top");
-          // imgCharacter.setAttribute("class", "item7")
-          imgCharacter.classList.add("thumbnail");
-          characterDiv.appendChild(imgCharacter);
-  
-          //Creamos un botón que enlace con su wiki
-  
-          let wikiBtn = document.createElement("button");
-          wikiBtn.setAttribute("class", "btn btn-primary");
-          wikiBtn.innerHTML = `<a href="${hero.urls[0].url}" class="btn btn-primary btn-lg btn-block">CONOCE MAS SOBRE TU PERSONAJE</a>`;
-          characterDiv.appendChild(wikiBtn);
-        }
-    
-      };
     });
 };
